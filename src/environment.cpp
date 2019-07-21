c62cb058043a48807e9459baad8d1c0622162563
saka1s <saka1s.jp@gmail.com>
2019-07-21 14:03:39 +0000

    Lesson1.Introdution to Lidar and Point Clouds

--- src/environment.cpp
+++ src/environment.cpp
@@ -42,13 +42,16 @@ void simpleHighway(pcl::visualization::PCLVisualizer::Ptr& viewer)
     // ----------------------------------------------------
     
     // RENDER OPTIONS
-    bool renderScene = true;
+    bool renderScene = false;
     std::vector<Car> cars = initHighway(renderScene, viewer);
     
     // TODO:: Create lidar sensor 
-
+    Lidar* lidar = new Lidar(cars, 0.);
     // TODO:: Create point processor
-  
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud;
+    cloud = lidar->scan();
+    // renderRays(viewer, lidar->position, cloud);
+    renderPointCloud(viewer, cloud, "cloud", Color(255, 255, 255));
 }
 
 
--- src/sensors/lidar.h
+++ src/sensors/lidar.h
@@ -90,17 +90,17 @@ struct Lidar
 		maxDistance = 50;
 		resoultion = 0.2;
 		// TODO:: set sderr to 0.2 to get more interesting pcd files
-		sderr = 0.0;
+		sderr = 0.2;
 		cars = setCars;
 		groundSlope = setGroundSlope;
 
 		// TODO:: increase number of layers to 8 to get higher resoultion pcd
-		int numLayers = 3;
+		int numLayers = 8;
 		// the steepest vertical angle
 		double steepestAngle =  30.0*(-pi/180);
 		double angleRange = 26.0*(pi/180);
 		// TODO:: set to pi/64 to get higher resoultion pcd
-		double horizontalAngleInc = pi/6;
+		double horizontalAngleInc = pi/64;
 
 		double angleIncrement = angleRange/numLayers;
 

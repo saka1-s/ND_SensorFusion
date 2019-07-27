324baae46d3815c42f8614664855ca409f7776e4
saka1-s <saka1s.jp@gmail.com>
2019-07-27 10:25:48 +0000

    insert Points to kd-tree

--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -17,6 +17,32 @@ struct Node
 	{}
 };
 
+void insertHelper(Node **node, std::vector<float> point, int id,int depth)
+{
+
+	if(*node == NULL)
+	{
+		*node = new Node(point, id);
+		return;
+	}
+	bool left;
+	if (depth%2 == 0) {
+		// compare x
+		left = point[0] < (*node)->point[0];
+	} else {
+		// comapre y
+		left = point[1] < (*node)->point[1];
+	}
+	if(left)
+	{
+		insertHelper(&(*node)->left, point, id, depth+1);
+	}
+	else
+	{
+		insertHelper(&(*node)->right, point, id, depth+1);
+	}
+}
+
 struct KdTree
 {
 	Node* root;
@@ -29,11 +55,11 @@ struct KdTree
 	{
 		// TODO: Fill in this function to insert a new point into the tree
 		// the function should create a new node and place correctly with in the root 
-
+		insertHelper(&root, point, id, 0);
 	}
 
-	// return a list of point ids in the tree that are within distance of pivot
-	std::vector<int> search(std::vector<float> pivot, float distanceTol)
+	// return a list of point ids in the tree that are within distance of target
+	std::vector<int> search(std::vector<float> target, float distanceTol)
 	{
 		std::vector<int> ids;
 		return ids;
